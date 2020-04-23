7a47b551ed7db33993e771379b8b3daf39e0c6f1
contra <yo@contra.io>
2020-04-23 18:07:24 -0400

    fix: better report command

--- bin/report.js
+++ bin/report.js
@@ -5,16 +5,17 @@ const incr = (v=0, v2=1) => v + v2
 const vals = Object.values(plan.value)
 const metrics = vals.reduce((acc, plan) => {
   if (plan.response) {
-    acc[plan.response] = incr(acc[plan.response])
+    acc[`responded ${plan.response}`] = incr(acc[`responded ${plan.response}`])
   } else {
-    acc['no response yet'] = incr(acc['no response yet'])
+    if (plan.attempts !== 0) {
+      acc['called with no response'] = incr(acc['called with no response'])
+    }
   }
   if (plan.attempts !== 0) acc['calls made'] = incr(acc['calls made'], plan.attempts)
   return acc
 }, {})
 
 console.log('=== Current Status ===')
-console.log('Total on the list:', vals.length)
 Object.entries(metrics).forEach(([ k, v ]) => {
   console.log(`${capitalize.words(k)}: ${v}`)
 })
