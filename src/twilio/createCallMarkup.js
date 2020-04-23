069717ef2d3ec5294284dfe7e10f837b2e80d9af
contra <yo@contra.io>
2020-04-22 21:08:25 -0400

    fix: better voice

--- src/twilio/createCallMarkup.js
+++ src/twilio/createCallMarkup.js
@@ -2,8 +2,9 @@ const { template } = require('lodash')
 const { twiml } = require('twilio')
 const prompt = require('../../config/prompt')
 
+const sayOpt = { language: prompt.language, voice: prompt.voice }
+
 module.exports.closing = () => {
-  const sayOpt = { language: prompt.language }
   const response = new twiml.VoiceResponse()
   response
     .say(prompt.closing, sayOpt)
@@ -12,7 +13,6 @@ module.exports.closing = () => {
 }
 
 module.exports.opening = (business, responseUrl) => {
-  const sayOpt = { language: prompt.language }
   const options = prompt.options.map((o) =>
     `If ${o.label} please press ${o.key}`
   ).join('. ')
